4aec82ad3e3c657699a0bde72b774989f3a88733
91738 <91738@DESKTOP-JE2TPP7>
2023-06-04 23:01:13 +0530

    [GAURI]: Encapsulation code

---
+++ src/Encapsulation/Ex1.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+public class Ex1 
+{
+	private int a;
+	
+	
+	public Ex1()
+	{
+		a=4;
+	}
+	
+	
+	public void SquareOfNum() 
+	{
+	    System.out.println(a*a);	
+	}
+	
+
+}
---
+++ src/Encapsulation/Test_Ex1.java
@@ -0,0 +1,13 @@
+package Encapsulation;
+
+public class Test_Ex1
+{
+	public static void main(String[] args) 
+	{
+		Ex1 obj= new Ex1();
+		obj.SquareOfNum();
+		
+		
+	}
+
+}
